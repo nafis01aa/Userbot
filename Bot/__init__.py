e5c73f6c50b649c4e9e653e8831c375ad517b5a7
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-07 23:31:21 +0600

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,14 +1,19 @@
 # Made by Nafis
-
 import sys
+import json
 import time
 import logging
 from pyrogram import Client, filters
 
 try:
     with open('config.json', 'r') as f:
-        configs = f.read()
+        configs = json.load(f)
 except:
     print('Config.json file is missing! Please check.')
+    sys.exit(1)
+
+BOT_TOKEN = configs.get('BOT_TOKEN', None)
+if not BOT_TOKEN:
+    print('Please fill the BOT_TOKEN variable in config.json file!')
     sys.exit(1)
 
