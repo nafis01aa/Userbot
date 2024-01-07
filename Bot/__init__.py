121fe352bbc3625604bc69be753bb49069742c40
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 00:10:12 +0600

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,9 +1,11 @@
-# Made by Nafis
+import os
 import sys
 import json
 import time
 import logging
 from pyrogram import Client, filters
+
+os.system('cls || clear')
 
 try:
     with open('config.json', 'r') as f:
@@ -17,3 +19,6 @@ if not BOT_TOKEN:
     print('Please fill the BOT_TOKEN variable in config.json file!')
     sys.exit(1)
 
+bot = Client('TGUserBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+bot.start()
