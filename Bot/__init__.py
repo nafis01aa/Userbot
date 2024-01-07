4b88cd995f64bf8cd46bb26edde5514411469516
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-07 23:25:45 +0600

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,1 +1,14 @@
-# Write from here
+# Made by Nafis
+
+import sys
+import time
+import logging
+from pyrogram import Client, filters
+
+try:
+    with open('config.json', 'r') as f:
+        configs = f.read()
+except:
+    print('Config.json file is missing! Please check.')
+    sys.exit(1)
+
