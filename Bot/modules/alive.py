c224c80b8e375d0e79b17e678adcf468c86733c4
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 20:54:50 +0600

    Update alive.py

--- Bot/modules/alive.py
+++ Bot/modules/alive.py
@@ -6,13 +6,13 @@ from Bot.funcs.fstools import get_time
 from Bot import user, logger, starting_time
 
 async def alive(_, message):
-    start_time = time()
+    start_time = int(round(time() * 1000))
     await message.edit('`..`')
-    end_time = time()
+    end_time = int(round(time() * 1000))
     uptime = get_time(time() - starting_time)
     alive_msg = (
         f'**--I Am Online--**\n\n'
         f'**UPTIME:-** {uptime}\n'
         f'**STATUS:-** Cool 🔥\n'
-        f'**PING:-** {} ms'
+        f'**PING:-** {end_time - start_time} ms'
     )
