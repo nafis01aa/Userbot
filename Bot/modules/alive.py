ef488cb9c0bb720deee9563de4245241f61ab37c
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 20:34:31 +0600

    Update alive.py

--- Bot/modules/alive.py
+++ Bot/modules/alive.py
@@ -1,1 +1,13 @@
+from time import time, sleep
+from pyrogram import filters
+from pyrogram.handlers import MessageHandler
 
+from Bot import user, logger, pm_hours
+
+async def alive(_, message):
+    uptime = 
+    await message.edit('`..`')
+    alive_msg = (
+        f'**--I Am Alive--**\n\n'
+        f'**UPTIME:- {}'
+    )
