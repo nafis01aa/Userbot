25dc1127eb423b4e7b8ccf97f8b9b76d06e677b8
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 20:53:01 +0600

    Update alive.py

--- Bot/modules/alive.py
+++ Bot/modules/alive.py
@@ -2,14 +2,17 @@ from time import time, sleep
 from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 
-from Bot import user, logger
 from Bot.funcs.fstools import get_time
+from Bot import user, logger, starting_time
 
 async def alive(_, message):
-    right_now = time() - 
-    uptime = get_time()
+    start_time = time()
     await message.edit('`..`')
+    end_time = time()
+    uptime = get_time(time() - starting_time)
     alive_msg = (
-        f'**--I Am Alive--**\n\n'
-        f'**UPTIME:- {}'
+        f'**--I Am Online--**\n\n'
+        f'**UPTIME:-** {uptime}\n'
+        f'**STATUS:-** Cool 🔥\n'
+        f'**PING:-** {} ms'
     )
