80255ca9fbbb34ab1817f9d3f20d1358a7670c04
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 18:00:19 +0600

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,3 +1,28 @@
-from Bot import bot
+from pyrogram import filters
+from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 
-bot.idle()
+from Bot import user, bot, starting_time, logger
+
+async def start(client, message):
+    text = (
+        f'**Welcome boss!**'
+    )
+    await message.edit(text)
+
+async def botstart(client, message):
+    text = (
+        f'**Welcome To Userbot!**'
+    )
+    await message.reply(text)
+
+async def main():
+    if bot:
+        bot.add_handler(MessageHandler(botstart, filters=filters.command('start')))
+    user.add_handler(MessageHandler(start, filters=filters.command('start')))
+
+if bot:
+    bot.loop.run_until_complete(main())
+    bot.loop.run_forever()
+else:
+    user.loop.run_until_complete(main())
+    user.loop.run_forever()
