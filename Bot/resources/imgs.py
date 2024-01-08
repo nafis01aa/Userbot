fdad48cc04f49f4bc07da343cb010c93d681fd8d
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 21:07:50 +0600

    Update imgs.py

--- Bot/resources/imgs.py
+++ Bot/resources/imgs.py
@@ -1,1 +1,6 @@
-
+alive_imgs = [
+    "https://graph.org/file/11d1029274122a4fc64bc.jpg",
+    "https://graph.org/file/36f910ee841ccdcecf85b.jpg",
+    "https://graph.org/file/c5ad036a03e5a2ad60d17.jpg",
+    "https://graph.org/file/d8fdb55d0e39d18881664.jpg"
+]
