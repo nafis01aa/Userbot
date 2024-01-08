3c5bb8444fdca53fe8df4ed6f1b004c7c7c90855
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 19:13:45 +0600

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -4,6 +4,7 @@ import json
 import logging
 from time import time, sleep
 from pyrogram import Client, filters, enums
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 os.system('cls || clear')
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
@@ -21,11 +22,6 @@ except:
     logger.error('Config.json file is missing! Please check.')
     sys.exit(1)
 
-BOT_TOKEN = configs.get('BOT_TOKEN', None)
-if not BOT_TOKEN:
-    logger.warning('BOT_TOKEN variable is missing! Skipping...')
-    sleep(0.5)
-
 API_ID = configs.get('API_ID', None)
 if not API_ID:
     logger.error('Please fill the API_ID variable in config.json file!')
@@ -41,11 +37,20 @@ if not SESSION_STRING:
     logger.error('Please fill the SESSION_STRING variable in config.json file!')
     sys.exit(1)
 
+BOT_TOKEN = configs.get('BOT_TOKEN', None)
+if not BOT_TOKEN:
+    logger.warning('BOT_TOKEN variable is missing! Skipping...')
+    sleep(0.5)
+
 logger.info('Creating user client!')
 user = Client('TGUserCli', api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING, parse_mode=enums.ParseMode.MARKDOWN).start()
+user_loop = user.loop
+user_scheduler = AsyncIOScheduler(event_loop=user_loop)
 
 if BOT_TOKEN:
     logger.info('Creating bot client!')
     bot = Client('TGUserBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, parse_mode=enums.ParseMode.MARKDOWN).start()
+    bot_loop = bot.loop
+    bot_scheduler = AsyncIOScheduler(event_loop=bot_loop)
 else:
     bot = ''
