4b789a5f64de14fc9d2bca5151bd1edd3aecd343
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-08 17:45:48 +0600

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,15 +1,17 @@
 import os
 import sys
 import json
-import time
 import logging
+from time import time
 from pyrogram import Client, filters
 
 os.system('cls || clear')
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.FileHandler('logs.txt'), logging.StreamHandler()], level=logging.INFO)
 
+starting_time = time()
 logger = getLogger(__name__)
+logger.info('Starting deploy userbot...')
 
 try:
     with open('config.json', 'r') as f:
@@ -20,8 +22,7 @@ except:
 
 BOT_TOKEN = configs.get('BOT_TOKEN', None)
 if not BOT_TOKEN:
-    logger.error('Please fill the BOT_TOKEN variable in config.json file!')
-    sys.exit(1)
+    logger.warning('BOT_TOKEN variable is missing! Skipping...')
 
 API_ID = configs.get('API_ID', None)
 if not API_ID:
@@ -33,8 +34,14 @@ if not API_HASH:
     logger.error('Please fill the API_HASH variable in config.json file!')
     sys.exit(1)
 
-user = Client('TGUserCli', api_id=API_ID, api_hash=API_HASH, parse_mode=enums.ParseMode.MARKDOWN)
-bot = Client('TGUserBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, parse_mode=enums.ParseMode.MARKDOWN)
+SESSION_STRING = configs.get('SESSION_STRING', None)
+if not SESSION_STRING:
+    logger.error('Please fill the SESSION_STRING variable in config.json file!')
+    sys.exit(1)
 
-user.start()
-bot.start()
+user = Client('TGUserCli', api_id=API_ID, api_hash=API_HASH, parse_mode=enums.ParseMode.MARKDOWN).start()
+
+if BOT_TOKEN:
+    bot = Client('TGUserBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, parse_mode=enums.ParseMode.MARKDOWN).start()
+else:
+    bot = ''
