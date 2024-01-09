c908ae7b71f0c381889744462b27132ace7372ab
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-09 13:19:22 +0600

    Update mute.py

--- Bot/modules/mute.py
+++ Bot/modules/mute.py
@@ -4,3 +4,13 @@ from pyrogram.handlers import MessageHandler
 from Bot import user, logger
 
 async def mute(_, message):
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    else:
+        if (message.command) < 2:
+            await message.edit('`Reply to a user or provide user id`')
+            return
+
+        user_id = message.command[1]
+
+    b
