fd3ac197f086ff8452d195854dd0663286e73fa1
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-09 19:04:56 +0600

    Update mute.py

--- Bot/modules/mute.py
+++ Bot/modules/mute.py
@@ -1,5 +1,6 @@
 from time import sleep
 from pyrogram import filters, enums
+from datetime import datetime, timedelta
 from pyrogram.types import ChatPermissions
 from pyrogram.handlers import MessageHandler
 
@@ -7,7 +8,9 @@ from Bot import user, logger
 
 async def mute(_, message):
     if message.chat.type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        await message.edit("`This plugin works in groups or supergroups only!`")
+        await message.edit("`This plugin works in groups and supergroups only!`")
+        sleep(1)
+        await message.delete()
         return
 
     get_me = await user.get_chat_member(chat_id=message.chat.id, user_id="me")
@@ -19,23 +22,43 @@ async def mute(_, message):
     
     if message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
+        
+        if len(message.command) > 1:
+            duration = message.command[1]
+        else:
+            duration = None
     else:
-        if (message.command) < 2:
+        if len(message.command) < 2:
             await message.edit('`Reply to a user or provide user id`')
+            sleep(1)
+            await message.delete()
             return
 
         user_id = message.command[1]
+        
+        if len(message.command) > 2:
+            duration = message.command[2]
+        else:
+            duration = None
 
     try:
-        await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, permissions=ChatPermissions())
+        if duration:
+            await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, permissions=ChatPermissions(), until_date=(datetime.now() + timedelta(hours=int(duration))))
+        else:
+            await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, permissions=ChatPermissions())
+        
         user_link = f"[User](tg://user?id={user_id})"
-        await message.edit(f"{user_link} `is muted!`")
+        await message.edit(f"{user_link} `is muted! ⛔`")
+        sleep(3)
+        await message.delete()
     except Exception as e:
         await message.edit(f"`ERROR: {e}`")
 
 async def unmute(_, message):
     if message.chat.type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         await message.edit("`This plugin works in groups or supergroups only!`")
+        sleep(1)
+        await message.delete()
         return
 
     get_me = await user.get_chat_member(chat_id=message.chat.id, user_id="me")
@@ -48,8 +71,10 @@ async def unmute(_, message):
     if message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
     else:
-        if (message.command) < 2:
+        if len(message.command) < 2:
             await message.edit('`Reply to a user or provide user id`')
+            sleep(1)
+            await message.delete()
             return
 
         user_id = message.command[1]
@@ -58,6 +83,8 @@ async def unmute(_, message):
         await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, permissions=ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_other_messages=True))
         user_link = f"[User](tg://user?id={user_id})"
         await message.edit(f"{user_link} `is unmuted! ✅`")
+        sleep(3)
+        await message.delete()
     except Exception as e:
         await message.edit(f"`ERROR: {e}`")
 
