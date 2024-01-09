47275be0afa2763dc7d8873ee4b014c7321c2981
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-09 18:39:02 +0600

    Update mute.py

--- Bot/modules/mute.py
+++ Bot/modules/mute.py
@@ -27,7 +27,7 @@ async def mute(_, message):
         user_id = message.command[1]
 
     try:
-        await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, ChatPermissions())
+        await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, permissions=ChatPermissions())
         user_link = f"[User](tg://user?id={user_id})"
         await message.edit(f"{user_link} `is muted!`")
     except Exception as e:
@@ -55,7 +55,7 @@ async def unmute(_, message):
         user_id = message.command[1]
 
     try:
-        await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_other_messages=True))
+        await user.restrict_chat_member(chat_id=message.chat.id, user_id=user_id, permissions=ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_other_messages=True))
         user_link = f"[User](tg://user?id={user_id})"
         await message.edit(f"{user_link} `is unmuted! ✅`")
     except Exception as e:
