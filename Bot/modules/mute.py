acebef914b9965582d8c5ac4846382a14965655a
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-09 13:24:50 +0600

    Update mute.py

--- Bot/modules/mute.py
+++ Bot/modules/mute.py
@@ -4,7 +4,10 @@ from pyrogram.handlers import MessageHandler
 from Bot import user, logger
 
 async def mute(_, message):
-    if message.chat.type not in 
+    if message.chat.type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        await message.edit("`This plugin works in groups or supergroups only!`")
+        return
+    
     if message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
     else:
