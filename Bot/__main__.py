7908bca49fc299fda8392baab1052655fb35fbbc
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-09 22:09:29 +0600

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -6,7 +6,7 @@ from signal import signal, SIGINT
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 
 from Bot.tools import ban, mute
-from Bot.modules import alive, id, pmusers
+from Bot.plugins import alive, id, pmusers
 from Bot import user, user_scheduler, bot, bot_scheduler, starting_time, logger
 
 async def yo(client, message):
