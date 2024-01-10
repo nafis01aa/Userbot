e82e88952d0c9fb266f0ac87f5fd44e87a4626e8
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-10 14:36:59 +0600

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -9,23 +9,43 @@ from Bot.tools import ban, mute
 from Bot.plugins import alive, id, pmusers
 from Bot import user, user_scheduler, bot, bot_scheduler, starting_time, logger
 
-async def yo(client, message):
+async def yo(_, message):
     text = (
         f'**Yoo!**'
     )
     await message.edit(text)
 
-async def botstart(client, message):
+async def botstart(_, message):
     text = (
         f'**Welcome To Userbot!**'
     )
     await message.reply(text)
 
-async def ping(client, message):
+async def ping(_, message):
     start_time = int(round(time() * 1000))
     loading = await message.edit('`wait..`')
     end_time = int(round(time() * 1000))
     await message.edit(f'`{end_time - start_time}` ms')
+
+async def logs(_, message):
+    with open('logs.txt', 'r') as log:
+        logtextlines = log.read().splitlines()
+    
+    ind = 1
+    Loglines = ''
+    try:
+        while len(Loglines) <= 2500:
+            Loglines = f'{logtextlines[-ind]}\n{Loglines}'
+            
+            if ind == len(logFileLines):
+                break
+            
+            ind += 1
+        
+        log_text = Loglines
+        await message.edit(text=log_text, disable_web_page_preview=True)
+    except Exception as e:
+        logger.error(f"LOG ERROR: {e}")
 
 def exiting(signal, frame):
     try:
@@ -42,6 +62,7 @@ def exiting(signal, frame):
 
 async def main():
     user.add_handler(MessageHandler(yo, filters=(filters.me & filters.command(['yo'], ['/','.',',','!']))))
+    user.add_handler(MessageHandler(logs, filters=(filters.me & filters.command(['log','logs'], ['/','.',',','!']))))
     user.add_handler(MessageHandler(ping, filters=(filters.me & filters.command(['ping'], ['/','.',',','!']))))
     
     if bot:
