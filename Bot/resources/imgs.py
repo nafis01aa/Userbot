1a26105c1ead1f4b2db72e620c45e414ed7638f8
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-17 12:12:35 +0600

    Update imgs.py

--- Bot/resources/imgs.py
+++ Bot/resources/imgs.py
@@ -4,3 +4,11 @@ alive_imgs = [
     "https://graph.org/file/c5ad036a03e5a2ad60d17.jpg",
     "https://graph.org/file/d8fdb55d0e39d18881664.jpg"
 ]
+
+pm_warn_imgs = [
+    "https://graph.org/file/f4858b80dbc68df6efc1e.jpg",
+    "",
+    "",
+    "",
+    ""
+]
