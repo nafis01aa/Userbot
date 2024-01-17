e322f44938852a6e474f5aeb8a0cc6885c9616fd
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-17 19:00:53 +0600

    Update pmusers.py

--- Bot/plugins/pmusers.py
+++ Bot/plugins/pmusers.py
@@ -4,11 +4,13 @@ from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 
 from Bot import user, logger, pm_hours
+from Bot.funcs.asynctools import new_task
 from Bot.resources.imgs import pm_warn_imgs
 
 counted = {}
 previous_msg = {}
 
+@new_task
 async def on_pm(client, message):
     user_id = message.chat.id
     pm_hold = pm_hours * 3600
@@ -29,6 +31,7 @@ async def on_pm(client, message):
     warn_message = await message.reply_photo(photo=warn_photo, quote=True, caption="Please don't spam, I will come back soon!")
     previous_msg[user_id] = warn_message.id
 
+@new_task
 async def stop(client, message):
     _hold = pm_hours
     stop_warn = await message.edit(f"`Successfully stopped warning message for this chat for {_hold} hours`")
