b980e5566bad6df2ae7956c7e59181edf68805a3
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-01-17 20:04:55 +0600

    Update fstools.py

--- Bot/funcs/fstools.py
+++ Bot/funcs/fstools.py
@@ -1,7 +1,8 @@
+import sys
 from aiofiles.os import path as aiopath
 from aioshutil import rmtree as aiormtree
 
-from Bot import user, bot, logger
+from Bot import user, user_scheduler, bot, bot_scheduler, logger
 
 def get_time(seconds):
     periods = [('Day', 86400), ('Hour', 3600), ('Minute', 60), ('Second', 1)]
@@ -16,6 +17,19 @@ def get_time(seconds):
                 result += f'{int(period_value)} {period_name} '
             
     return result
+
+def exiting(signal, frame):
+    try:
+        if bot_scheduler:
+            if bot_scheduler.running:
+                bot_scheduler.shutdown(wait=False)
+        if user_scheduler.running:
+            user_scheduler.shutdown(wait=False)
+    except:
+        pass
+    
+    logger.info('Exiting deploy..!')
+    sys.exit(0)
 
 async def clean_download(path):
     if await aiopath.exists(path):
