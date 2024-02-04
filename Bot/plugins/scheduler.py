e3c77d7ec3050f34370e8b14a708601a4fb538d3
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 23:58:59 +0600

    Update scheduler.py

--- Bot/plugins/scheduler.py
+++ Bot/plugins/scheduler.py
@@ -7,6 +7,8 @@ from Bot.functions.fstools import get_time
 from Bot import user, user_scheduler, logger
 from Bot.functions.asynctools import new_task
 
+sorted_tasks = {}
+
 async def scheduler_task():
     pass
 
@@ -16,8 +18,40 @@ async def _schedule(_, message):
         await message.edit('`Reply to a message`')
         return
 
+    seconds = ''
+    minutes = ''
+    hours = ''
+    mode = ''
+    
+    if len(message.command) > 1:
+        try:
+            mode = 'hours'
+            seconds = int(message.command[1]) * 3600
+        except ValueError:
+            if 's' in message.command[1]:
+                mode = 'seconds'
+                seconds = int(message.command[1].replace('s', ''))
+            elif 'm' in message.command[1]:
+                mode = 'minutes'
+                minutes = int(message.command[1].replace('m', ''))
+            elif 'h' in message.command[1]:
+                mode = 'hours'
+                hours = int(message.command[1].replace('h', ''))
+        except Exception as e:
+            await message.edit(f'`{e}`')
+            return
+    else:
+        mode = 'hours'
+        hours = 1
+
     msg = message.reply_to_message
-    task_id = f'{message.reply_to_message.id}{randint(100, 999)}'
-    user_scheduler.add_job(scheduler_task, 'interval', (msg), 
+    task_id = user_scheduler.add_job(scheduler_task, 'interval', (msg),
+                           seconds=seconds if seconds else 0,
+                           minutes=minutes if minutes else 0,
+                           hours=hours)
+
+    content = msg.caption if msg.caption else msg.text
+    sorted_tasks[task_id] = content[:10]
+    await message.edit(f'`Post scheduled for every {} `')
 
 user.add_handler(MessageHandler())
