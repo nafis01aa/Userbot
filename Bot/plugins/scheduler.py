59a469a5caa9e4047f7db6b0c8a6e506836d706b
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 21:54:08 +0600

    Update scheduler.py

--- Bot/plugins/scheduler.py
+++ Bot/plugins/scheduler.py
@@ -2,10 +2,13 @@ from random import randint
 from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 
-from Bot import user, logger
 from Bot.utils.commands import UCommand
 from Bot.functions.fstools import get_time
+from Bot import user, user_scheduler, logger
 from Bot.functions.asynctools import new_task
+
+async def scheduler_task():
+    pass
 
 @new_task
 async def _schedule(_, message):
@@ -15,6 +18,6 @@ async def _schedule(_, message):
 
     msg = message.reply_to_message
     task_id = f'{message.reply_to_message.id}{randint(100, 999)}'
-    -
+    user_scheduler.add_job(scheduler_task, 'interval', (msg), 
 
 user.add_handler(MessageHandler())
