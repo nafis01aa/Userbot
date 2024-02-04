9ba79111f560169adb88002f341a524bfa42c08a
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 18:19:52 +0600

    Update commands.py

--- Bot/utils/commands.py
+++ Bot/utils/commands.py
@@ -1,20 +1,26 @@
 prefixes = ['/', '.', ',', '!']
 
-class UserCommands:
+class BotCommands:
     def __init__(self):
         self.help = (['help'], prefixes)
+        self.start = (['start'], prefixes)
+
+class UserCommands:
+    def __init__(self):
+        self.alive = (['alive'], prefixes)
+        self.ban = (['ban'], prefixes)
+        self.gban = (['gban'], prefixes)
+        self.help = (['help'], prefixes)
+        self.id = (['id'], prefixes)
+        self.log = (['log', 'logs'], prefixes)
+        self.mute = (['mute'], prefixes)
         self.ping = (['ping'], prefixes)
+        self.pmstop = (['stop'], prefixes)
+        self.purge = (['purge'], prefixes)
+        self.restrictforwarder = (['forward', 'copymsg'], prefixes)
+        self.unban = (['unban'], prefixes)
+        self.unmute = (['unmute'], prefixes)
         self.yo = (['yo'], prefixes)
 
-        self.alive = (['alive'], prefixes)
-        self.ban = ()
-        self.gban = ()
-        self.id = (['id'], prefixes)
-        self.mute = ()
-        self.pmstop = (['stop'], prefixes)
-        self.purge = ()
-        self.restrictforwarder = (['forward', 'copymsg'], prefixes)
-        self.unban = ()
-        self.unmute = ()
-
+BCommand = BotCommands()
 UCommand = UserCommands()
