ff01e0de978db534a0ce54cc1b6520ae89d61508
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 21:37:51 +0600

    Update scheduler.py

--- Bot/plugins/scheduler.py
+++ Bot/plugins/scheduler.py
@@ -1,2 +1,10 @@
+from pyrogram import filters
+from pyrogram.handlers import MessageHandler
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from Bot.utils.commands import UCommand
+from Bot.functions.fstools import get_time
+from Bot.resources.imgs import alive_imgs
+from Bot.functions.asynctools import new_task
+from Bot import user, logger, starting_time, user_full_name, user_userid
 
