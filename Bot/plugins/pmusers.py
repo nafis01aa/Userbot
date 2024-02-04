ab85f8eb61a7d80d2d39db1e12aa87fd113fa33b
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 17:04:23 +0600

    Update pmusers.py

--- Bot/plugins/pmusers.py
+++ Bot/plugins/pmusers.py
@@ -4,6 +4,7 @@ from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 
 from Bot import user, logger, pm_hours
+from Bot.utils.commands import UCommand
 from Bot.functions.asynctools import new_task
 from Bot.resources.imgs import pm_warn_imgs
 
@@ -32,7 +33,7 @@ async def on_pm(client, message):
     previous_msg[user_id] = warn_message.id
 
 @new_task
-async def stop(client, message):
+async def pmstop(client, message):
     _hold = pm_hours
     stop_warn = await message.edit(f"`Successfully stopped warning message for this chat for {_hold} hours`")
     user_id = message.chat.id
@@ -45,4 +46,4 @@ async def stop(client, message):
         pass
 
 user.add_handler(MessageHandler(on_pm, filters=(filters.private & ~filters.me)))
-user.add_handler(MessageHandler(stop, filters=(filters.private & filters.me & filters.command(['stop'], ['/','.',',']))))
+user.add_handler(MessageHandler(pmstop, filters=(filters.private & filters.me & filters.command(*UCommand.pmstop))))
