1d6d6f571a7b34560cda97482fd2cdc872655d77
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 17:55:41 +0600

    Update commands.py

--- Bot/utils/commands.py
+++ Bot/utils/commands.py
@@ -8,8 +8,14 @@ class BotCommands:
         self.yo = (['yo'], prefixes)
 
         self.alive = (['alive'], prefixes)
+        self.ban = ()
+        self.gban = ()
         self.id = (['id'], prefixes)
+        self.mute = ()
         self.pmstop = (['stop'], prefixes)
+        self.purge = ()
         self.restrictforwarder = (['forward', 'copymsg'], prefixes)
+        self.unban = ()
+        self.unmute = ()
 
 UCommand = BotCommands()
