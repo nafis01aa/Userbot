0699ed7cc6855a052eebd84301418fe1d8005e10
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-04 16:55:22 +0600

    Create commands.py

---
+++ Bot/utils/commands.py
@@ -0,0 +1,14 @@
+prefixes = ['/', '.', ',', '!']
+
+class BotCommands:
+    def __init__(self):
+        self.start = (['start'], prefixes)
+        self.help = (['help'], prefixes)
+        self.ping = (['ping'], prefixes)
+        self.yo = (['yo'], prefixes)
+
+        self.alive = (['alive'], prefixes)
+        self.pmstop = (['stop'], prefixes)
+        self.restrictforwarder = (['forward', 'copymsg'], prefixes)
+
+UCommand = BotCommands()
