e90f6d251191972b25feda96fdb8554cef2ed82f
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-10 17:40:17 +0600

    Update database.py

--- Bot/utils/database.py
+++ Bot/utils/database.py
@@ -2,9 +2,9 @@ from Bot import schedule_conn
 
 class UMdb:
     @staticmethod
-    async def insert_schedule_data(self, data):
+    async def insert_schedule_data(data):
         return await schedule_conn.insert_one(data)
 
     @staticmethod
-    async def remove_schedule_data(self, id):
+    async def remove_schedule_data(id):
         return await schedule_conn.remove_one({'_id': id})
