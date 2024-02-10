d6c69afb0b3f8ce19d8764a1a313ea039912ef93
Md Nafis Muhtadi <155907014+nafis01aa@users.noreply.github.com>
2024-02-10 10:30:46 +0600

    Update database.py

--- Bot/utils/database.py
+++ Bot/utils/database.py
@@ -1,1 +1,8 @@
+from Bot import schedule_conn
 
+class Mongodb:
+    async def insert_data(self, data):
+        return await schedule_conn.insert_one(data)
+
+    async def all(self):
+        pass
